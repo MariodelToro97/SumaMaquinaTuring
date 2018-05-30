3fad995484f6dde4ef25ea535125b4cc1d3b5292
Mario Josue del Toro Morales <mario_chomy@outlook.com>
2018-05-29 19:06:18 -0500

    Initial commit

---
+++ src/maquinaturing2/MaquinaTuring2.java
@@ -0,0 +1,26 @@
+/*
+* Este programa fue hecho por Mario Josue del Toro Morales
+* con Número de Control 15460644
+* Estudiante del sexto semestre de la carrera de Ingeniería en Sistemas Computacionales
+* del Instituto Tecnológico de Colima
+* Materia de Lenguajes y Autómatas I
+******Descripción************
+* Programa que usa la máquina de Turing para sacar la suma de dos números binarios
+ */
+package maquinaturing2;
+
+/**
+ *
+ * @author Mario Josue del Toro Morales
+ */
+public class MaquinaTuring2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Principal principal = new Principal(); // Declaración de la clase que tiene el formato gráfico
+        principal.setVisible(true); //Despliegue de la ventana gráfica
+    }
+    
+}
